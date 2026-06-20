ca9500dde21aca2a2730edad2a660b2b3952bc60
kwonhyeokjun <gurwns9035@naver.com>
2026-06-20 20:03:49 +0900

    test 5

--- blog_backend/src/main.ts
+++ blog_backend/src/main.ts
@@ -3,7 +3,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  console.log('1');
+  const app = await NestFactory.create(AppModule);
+  console.log('2');
 
   app.enableCors({
     origin: [
@@ -20,16 +22,18 @@ async function bootstrap() {
     }),
   )
 
-  const port = Number(process.env.PORT) || 4000
+  const port = Number(process.env.PORT) || 4000;
+  console.log('3', port);
 
-  console.log('process.env.PORT =', process.env.PORT)
-  console.log('process.env =', Object.keys(process.env))
+  console.log('process.env.PORT =', process.env.PORT);
+  console.log('process.env =', Object.keys(process.env));
 
-  console.log('PORT =', port)
+  console.log('PORT =', port);
 
-  await app.listen(port, '0.0.0.0')
+  await app.listen(port, '0.0.0.0');
+  console.log('4');
 
-  console.log(`Server running on ${port}`)
+  console.log(`Server running on ${port}`);
 }
 
 bootstrap()
